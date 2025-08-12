9476f997a8f54ad7333d3444e44d049a1db5711f
WriteStone <139120755+WriteStone@users.noreply.github.com>
2025-08-12 16:58:53 +0800

    Delete 07-MultiDeviceCommunication-master directory

--- 07-MultiDeviceCommunication-master/products/phone/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/home/Index.ts
+++
@@ -1,1 +0,0 @@
-export { Index as Home } from "@bundle:com.example.multdevicecommunication/phone@home/ets/pages/Index";
--- 07-MultiDeviceCommunication-master/products/phone/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/socialCircle/Index.ts
+++
@@ -1,1 +0,0 @@
-export { SocialCircle } from "@bundle:com.example.multdevicecommunication/phone@socialCircle/ets/pages/Index";
